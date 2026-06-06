cd9d99d84a84c3134176e9b838ecaadd2903f6e3
austinatogo1 <austin.atogo1@student.moringaschool.com>
2026-06-06 23:54:43 +0300

    chore: add gh-pages deploy script

--- coffee-r-us/vite.config.js
+++ coffee-r-us/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/https://github.com/austinatogo1/coffee-r-us/tree/main/',   
   test: {
     environment: "jsdom",
     globals: true,
